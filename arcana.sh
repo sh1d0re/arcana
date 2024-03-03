75753f0a56eaf374a1474602ad25a911cc6af49c
Shidore <82015990+sh1d0re@users.noreply.github.com>
2024-03-03 19:04:51 +0900

    Update arcana.sh

--- arcana.sh
+++ arcana.sh
@@ -53,8 +53,12 @@ print("━━━━━━━━━━━━━━━━━━━━━━┳━�
 try:
 	MongoClient("mongodb+srv://arcana_user:WVV62CYB3g5eJcqL@arcana.c8ozcdl.mongodb.net/?retryWrites=true&w=majority&appName=arcana").admin.command("ping")
 except Exception as e:
-	print("                Error ┖╴\x1b[31m"+str(e)+"\nProgram aborted. Contact developer at github repository or admin.")
-	quit()
+ 	print("                Error ┠╴\x1b[31m"+str(e)+"\x1b[39;49m")
+	if input(" Install SSL? [Y/N]:  ┖╴ ") == "Y":
+		os.system("""bash install_certifications.command""")
+  	else:
+		print("                Error ┖╴\x1b[31m"+str(e)+"\nProgram aborted. Contact developer at github repository or admin.")
+		quit()
 print("        Server Module ┖╴\x1b[32mSUCCESS\x1b[39;49m")
 print("━━━━━━━━━━━━━━━━━━━━━━┳━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n[ 3: LOADING ARCANA ] ┃ Run \x1b[0;1m/help\x1b[0m Or Read \x1b[0;1mREADME.md\x1b[0m For Info.")
 cluster,uri="",""
@@ -93,6 +97,54 @@ while True:
 		else:print("Code is not valid. Try checking for mistypes.")
 	msg={"id": b2e.encode(str(encode), id_), "message": b2e.encode(str(encode), str(sndmsg)), "date": b2e.encode(str(encode),date),"deltatime": b2e.encode(str(encode),deltatime) , "channel": b2e.encode(str(encode),"VALID")}
 	db.insert_one(msg)''' > main.py
+echo '''#!/bin/sh
+
+/Library/Frameworks/Python.framework/Versions/3.10/bin/python3.10 << "EOF"
+
+# install_certifi.py
+#
+# sample script to install or update a set of default Root Certificates
+# for the ssl module.  Uses the certificates provided by the certifi package:
+#       https://pypi.org/project/certifi/
+
+import os
+import os.path
+import ssl
+import stat
+import subprocess
+import sys
+
+STAT_0o775 = ( stat.S_IRUSR | stat.S_IWUSR | stat.S_IXUSR
+             | stat.S_IRGRP | stat.S_IWGRP | stat.S_IXGRP
+             | stat.S_IROTH |                stat.S_IXOTH )
+
+def main():
+    openssl_dir, openssl_cafile = os.path.split(
+        ssl.get_default_verify_paths().openssl_cafile)
+
+    print(" -- pip install --upgrade certifi")
+    subprocess.check_call([sys.executable,
+        "-E", "-s", "-m", "pip", "install", "--upgrade", "certifi"])
+
+    import certifi
+
+    # change working directory to the default SSL directory
+    os.chdir(openssl_dir)
+    relpath_to_certifi_cafile = os.path.relpath(certifi.where())
+    print(" -- removing any existing file or link")
+    try:
+        os.remove(openssl_cafile)
+    except FileNotFoundError:
+        pass
+    print(" -- creating symlink to certifi certificate bundle")
+    os.symlink(relpath_to_certifi_cafile, openssl_cafile)
+    print(" -- setting permissions")
+    os.chmod(openssl_cafile, STAT_0o775)
+    print(" -- update complete")
+
+if __name__ == "__main__":
+    main()
+EOF''' >>> install_certifications.command
 mkdir b2eC
 cd b2eC ; echo '''
 # This script of b2eC is specifically and heavily modified for ARCANA. I would not reccomend any practical usages.
